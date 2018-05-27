dab1c7c0c51e5f17913c5cc942a8dab12be13556
Matt Farmer <mrfarmer777@gmail.com>
2018-05-27 18:22:07 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,12 @@
 require 'pry'
 
 def my_find(collection)
-
+  i=0
+  res=[]
+  while i < collection.length
+    res<<yield(collection[i])
+    i+=1
+  end
+  #return the first element from the original collection that evaluates to true in the results array
+  collection[res.index(true)]
 end
